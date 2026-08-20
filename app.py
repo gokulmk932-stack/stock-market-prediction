cf19a4f73af8af4e7b6c5043d0523b72ace22743
gokulmk932-stack <gokulmk932@gmail.com>
2026-08-20 18:03:53 +0530

    Add files via upload
    upload stock market prediction project

--- app.py
+++ app.py
@@ -295,7 +295,6 @@ def dashboard():
 if "logged_in" not in st.session_state:
     st.session_state.logged_in = False
 
-
 if st.session_state.logged_in:
 
     dashboard()
@@ -308,7 +307,7 @@ else:
     )
 
     if option == "Login":
-
+ 
         login_page()
 
     else:
